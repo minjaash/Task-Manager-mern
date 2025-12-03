e2699a16dc5250a12fefb0b23139344de0ea4f5d
AASH777 <amminj777@gmail.com>
2025-12-03 17:58:41 +0530

    removing the token if already exits

--- task-manager/src/components/Home.jsx
+++ task-manager/src/components/Home.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Home = () => {
+  useEffect(()=>{
+    if(localStorage.getItem("user"))
+      localStorage.removeItem("user")
+  })
   return (
     <div>
         <p>Welcome to Task Manager.Please register or Login to continue. </p> 
